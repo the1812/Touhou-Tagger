7f0855191d5c92465281f75b647f7c7695bcdd41
the1812 <the1812@outlook.com>
2019-09-23 18:42:52 +0800

    Export mappings

--- src/core/index.ts
+++ src/core/index.ts
@@ -1,5 +1,7 @@
 export * from './writer/metadata-writer'
 export * from './writer/mp3-writer'
+export * from './writer/writer-mappings'
 export * from './metadata/metadata'
 export * from './metadata/metadata-source'
+export * from './metadata/source-mappings'
 export * from './metadata/thb-wiki'
