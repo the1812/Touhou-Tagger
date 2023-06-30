5772f679fb476b9e85a6e8a6379ad468cc78acb7
the1812 <the1812@outlook.com>
2023-06-30 23:26:43 +0800

    Delete unused files

--- src/flac-tagger/metadata-block/index.ts
+++
@@ -1,9 +0,0 @@
-import { BufferBase } from '../buffer-base'
-import type { MetadataBlockHeader } from './header'
-
-export abstract class MetadataBlock extends BufferBase {
-  abstract header: MetadataBlockHeader
-  get type() {
-    return this.header.type
-  }
-}
