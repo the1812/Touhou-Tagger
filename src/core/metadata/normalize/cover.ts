b6410fe68f34e9bf01df304f6f4a4acdbda54ad7
the1812 <the1812@outlook.com>
2023-08-04 21:49:11 +0800

    Remove remote cover download in fetchCoverPlugin

--- src/core/metadata/normalize/cover.ts
+++ src/core/metadata/normalize/cover.ts
@@ -1,31 +1,18 @@
-import Axios from 'axios'
 import { MetadataNormalizePlugin } from './normalize'
 
 /** 处理封面图片 */
-export const fetchCoverPlugin: MetadataNormalizePlugin = ({ cover, config }) => {
+export const fetchCoverPlugin: MetadataNormalizePlugin = ({ cover }) => {
   let firstCoverBuffer: Buffer | undefined
-  const downloadRemoteCover = async (url: string) => {
-    return Axios.get<Buffer>(url, {
-      responseType: 'arraybuffer',
-      timeout: config.timeout * 1000,
-    })
-  }
   return async ({ metadata, index }) => {
     if (index === 0) {
       if (cover !== undefined) {
         firstCoverBuffer = cover
-      } else if (typeof metadata.coverImage === 'string') {
-        const response = await downloadRemoteCover(metadata.coverImage)
-        firstCoverBuffer = response.data
       }
       metadata.coverImage = firstCoverBuffer
     }
     if (index > 0) {
       if (metadata.coverImage === undefined && firstCoverBuffer !== undefined) {
         metadata.coverImage = firstCoverBuffer
-      } else if (typeof metadata.coverImage === 'string') {
-        const response = await downloadRemoteCover(metadata.coverImage)
-        metadata.coverImage = response.data
       }
     }
   }
